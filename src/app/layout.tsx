b4cadad99731616b23b6e21cb507992679676edd
Luis Enrique Galvan Morales <lusen121502@gmail.com>
2026-05-30 12:08:57 -0500

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "next-shadcn-ui",
+  description: "Dashboard de Proyectos con shadcn/ui",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="es"
+      className="h-full antialiased font-sans"
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
